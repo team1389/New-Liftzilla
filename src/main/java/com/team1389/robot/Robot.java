63e97cdea7729bfe43f9b4e2ea50d8fb45ca003a
Quunii <kennethmiuraubw@gmail.com>
2018-01-16 18:14:58 -0500

    initial commit

---
+++ src/main/java/com/team1389/robot/Robot.java
@@ -0,0 +1,45 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package com.team1389.robot;
+import com.team1389.hardware.outputs.hardware.CANTalonHardware;
+import com.team1389.hardware.outputs.software.PercentOut;
+import com.team1389.hardware.registry.Registry;
+import com.team1389.hardware.registry.port_types.CAN;
+
+import edu.wpi.first.wpilibj.IterativeRobot;
+
+/**
+ * This is a demo program showing the use of the RobotDrive class. The
+ * SampleRobot class is the base of a robot application that will automatically
+ * call your Autonomous and OperatorControl methods at the right time as
+ * controlled by the switches on the driver station or the field controls.
+ *
+ * <p>The VM is configured to automatically run this class, and to call the
+ * functions corresponding to each mode, as described in the SampleRobot
+ * documentation. If you change the name of this class or the package after
+ * creating this project, you must also update the build.properties file in the
+ * project.
+ *
+ * <p>WARNING: While it may look like a good choice to use for your code if
+ * you're inexperienced, don't. Unless you know what you are doing, complex code
+ * will be much more difficult under this system. Use IterativeRobot or
+ * Command-Based instead if you're new.
+ */
+public class Robot extends IterativeRobot {
+	CANTalonHardware talon = new CANTalonHardware(false, new CAN(10), new Registry());
+	PercentOut setter = talon.getVoltageController();
+	@Override
+	public void robotInit() {
+
+	}
+	@Override
+	public void teleopPeriodic() {
+		setter.set(1);
+	}
+
+}
